dfbe7a6230901bdebe40eaf31586e067a86733c7
OCY <ok0523ok@gmail.com>
2023-04-04 11:43:21 +0900

    OCY Lecture05 update & 03,04 README.md modify

---
+++ Lecture05/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,164 @@
+﻿//#define DEBUG
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+// game_state == 0 일때
+int print_title_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*              지렁이 게임               *" << std::endl;
+    std::cout << "*             (Snake  Bite)              *" << std::endl;
+#ifdef DEBUG
+    std::cout << "*            - 디버그 모드 -             *" << std::endl;
+#else
+    std::cout << "*                                        *" << std::endl;
+#endif  
+    std::cout << "*   1. 게임 시작                         *" << std::endl;
+    std::cout << "*   2. 게임 설명                         *" << std::endl;
+    std::cout << "*   3. 게임 랭킹 보기                    *" << std::endl;
+    std::cout << "*   4. 게임 종료 (esc)                   *" << std::endl;
+    std::cout << "******************************************" << std::endl;
+
+    return 0;
+}
+
+void gotoxy(int x, int y)
+{
+    //x, y 좌표 설정
+    COORD pos = { x,y };
+
+    //커서 이동
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+// game_state == 1 일때
+int print_game_screen(int stage_width, int stage_height)
+{
+    system("cls");
+
+    //위쪽 벽 - width 만큼 *(벽) 출력 후 줄바꿈
+    for (int i = 0; i < stage_width; i++)
+    {
+        std::cout <<"*";
+    }
+
+    std::cout<<std::endl;
+
+    //양쪽 벽 - 위,아래 벽을 제외한 (height-2)줄 만큼 *(벽) width-2 만큼의 공백 *(벽) 줄바꿈 반복
+    for (int i = 0; i < stage_height-2; i++)
+    {
+        std::cout << "*";
+
+        for (int i = 0; i < stage_width-2; i++)
+        {
+            std::cout << " ";
+        }
+
+        std::cout << "*"<<std::endl;
+    }
+
+    //아래쪽 벽 - width 만큼 *(벽) 출력
+    for (int i = 0; i < stage_width; i++)
+    {
+        std::cout << "*";
+    }
+
+    gotoxy(stage_width ,stage_height);
+    
+    return 0;
+}
+
+// game_state == 2 일때
+int print_introduction_screen()
+{
+    std::cout << "******************************************" << std::endl;
+    std::cout << "*       Ooo 지렁이 키우기 게임 ooo       *" << std::endl;
+    std::cout << "*                                        *" << std::endl;
+    std::cout << "*   @를 먹으며 커지는 지렁이를 조작하여  *" << std::endl;
+    std::cout << "*   최대한 크게 키워보세요!              *" << std::endl;
+    std::cout << "******************************************" << std::endl;
+    std::cout << std::endl;
+    std::cout << "타이틀화면으로 돌아갈까요? (Y/N)" << std::endl;
+
+    return 0;
+}
+
+int main()
+{   
+    int game_state = 0;
+    int is_game_running = 1;
+
+    while (is_game_running)
+    {
+        char key_input = '0';
+
+        switch (game_state)
+        {
+            case 0:
+                print_title_screen();
+                key_input = _getch();
+
+        goto1:  switch (key_input)
+                {
+                    case '1':
+                        game_state = 1;
+                        break;
+
+                    case '2':
+                        game_state = 2;
+                        break;
+
+                    case '3':
+                        break;
+
+                    case '4':
+                        is_game_running = 0;
+                        break;
+
+                    case 27:
+                        is_game_running = 0;
+                        break;
+
+                    default:
+                        while (key_input!=49 && key_input != 50 && key_input != 51 && key_input != 52 && key_input != 27)
+                        {
+                            key_input = _getch();
+                        }
+                        goto goto1;
+                }
+            break;
+
+        case 1:
+            print_game_screen(30, 15);
+            key_input = _getch();
+            
+        case 2:
+            system("cls");
+            print_introduction_screen();
+            key_input = _getch();
+    goto2:  switch (key_input)
+            {
+            case 'y':
+                game_state = 0;
+                break;
+            case 'n':
+                break;
+            default:
+                while (key_input != 89 && key_input != 121 && key_input != 78 && key_input != 110)
+                {
+                    key_input = _getch();
+                }
+                goto goto2;
+                break;
+            }
+
+        default:
+            break;
+        }
+
+    }
+    return 0;
+}
---
+++ Lecture05/README.md
@@ -0,0 +1,7 @@
+# Lecture05 : visual studio 에서 c++ 언어로 지렁이 게임의 일부를 만들기
+* 교수님이 올리신 지렁이 코드를 가져와 열심히 바라보았다..
+* COORD 변수; 콘솔 화면 좌표를 담는 구조체
+* SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), 변수); 마우스 커서 이동(기본핸들(기본핸들),좌표)..?
+* system("cls"); 로 콘솔 화면에 텍스트 지우기 가능
+* 과제는 for문이랑 cls를 사용해서 작성하였다.
+* 끝내고 chatgpt 한테 스네이크 게임 c++로 만들어 달라고 하니 만들어 주었다 하하.. ㅋㅋ.....
