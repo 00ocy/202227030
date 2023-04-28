6634b44ff3cfe3216bb77ec1a681c14a9076ec89
00ocy <ok0523ok@gmail.com>
2023-04-28 21:33:18 +0900

    4/28 8주차과제끝

--- Lecture08/ConsoleApplication5/ConsoleApplication5/MGameLoop.hpp
+++ Lecture08/ConsoleApplication5/ConsoleApplication5/MGameLoop.hpp
@@ -18,14 +18,15 @@
 
 namespace MuSoeun
 {
-	void gotoxy(int x, int y) {
+	void gotoxy(int x, int y)
+	{
 		//x, y 좌표 설정
 		COORD pos = { x,y };
 		//커서 이동
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 	}
 
-	void SetCursorState(bool visible)
+	void SetCursorState(bool visible) //커서 안 보이게 하기
 	{
 		/*CONSOLE_CURSOR_INFO consoleCursorInfo;
 		consoleCursorInfo.bVisible = visible;
@@ -38,10 +39,11 @@ namespace MuSoeun
 	{
 	public:
 		MGameLoop() {}
-		~MGameLoop() {}
+		~MGameLoop() {} 
 
-		bool isGameRunning = false;
-		bool OnOff = false;
+		bool isGameRunning = false;    //게임실행 체크
+		bool exitselect = false;       //ESC눌렀을때 체크
+		bool OnOff = true;             //끄냐 마냐 체크
 
 		void Initialize()
 		{
@@ -50,25 +52,30 @@ namespace MuSoeun
 			gotoxy(3, 15);
 			std::cout << ANSI_COLOR_RESET"게임 초기화중" << std::endl;
 			isGameRunning = true;
-			OnOff = true;
+			exitselect = true;
 		}
+
 		void Release()
 		{
-			gotoxy(5, 5);
-			std::cout << ANSI_COLOR_RESET"종료하시dddddddd겠습니까?" << std::endl;
+			system("cls");
+			gotoxy(5, 7);
+			std::cout << ANSI_COLOR_RESET"종료하시겠습니까?" << std::endl;
 			std::cout << "               " << std::endl;
-			std::cout << "     [ 예 ]   [ 아니오 ]" << std::endl;
-						
+			std::cout << ANSI_COLOR_YELLOW"     [ 예 ]";
+			std::cout << ANSI_COLOR_RESET"   [ 아니오 ]" << std::endl;
+			
+			while (exitselect)
+			{
+				if (_kbhit()){	KeyEvent2(_getch()); }
+			}						
 		}
 
 		void Update()
 		{
 			gotoxy(3, 16);
 			std::cout << ANSI_COLOR_RESET"게임 키입력 대기" << std::endl;
-			if (_kbhit())
-			{
-				KeyEvent(_getch());
-			}						
+
+			if (_kbhit()){ KeyEvent(_getch()); }						
 		}
 
 		void Render()
@@ -79,6 +86,7 @@ namespace MuSoeun
 
 		void Run()
 		{
+			system("cls");
 			Initialize();
 			while (isGameRunning)
 			{
@@ -94,41 +102,30 @@ namespace MuSoeun
 			{
 			case KEY_ESC:
 				gotoxy(5, 5);
-				std::cout << ANSI_COLOR_RESET"ESC 눌림         " << std::endl;
-
-				gotoxy(5, 7);
-				std::cout << ANSI_COLOR_RESET"종료하시겠습니까?" << std::endl;
-				std::cout << "               " << std::endl;
-				std::cout << "     [ 예 ]   [ 아니오 ]" << std::endl;
-				
-				while (OnOff)
-				{
-					if (_kbhit())
-					{						
-						KeyEvent2(_getch());
-					}
-				}
+				Release();
 				break;
 
-			case KEY_LEFT:
-				
+			case KEY_LEFT:				
 				gotoxy(5, 5);
 				std::cout << ANSI_COLOR_RED"왼쪽 눌림  " << std::endl;
 				break;
+
 			case KEY_RIGHT:
 				gotoxy(5, 5);
 				std::cout << ANSI_COLOR_GREEN"오른쪽 눌림" << std::endl;
 				break;
+
 			case KEY_ENTER:
 				gotoxy(5, 5);
 				std::cout << ANSI_COLOR_BLUE"엔터 눌림  " << std::endl;
 				break;
+
 			default:
 				isGameRunning = true;
 				break;
-			}
-			
+			}			
 		}
+
 		void KeyEvent2(char KeyInput2)
 		{
 			switch (KeyInput2)
@@ -137,31 +134,29 @@ namespace MuSoeun
 					gotoxy(5, 9);
 					std::cout << ANSI_COLOR_YELLOW"[ 예 ]";
 					std::cout << ANSI_COLOR_RESET"   [ 아니오 ]" << std::endl;
-					
-					while (OnOff)
-					{
-						if (_kbhit()&&_getch() == KEY_ENTER)
-						{
-							OnOff = false;
-							isGameRunning = false;
-						}
-					}
+					OnOff = true;					
 					break;
 
 				case KEY_RIGHT:
 					gotoxy(5, 9);
 					std::cout << ANSI_COLOR_RESET"[ 예 ]";
 					std::cout << ANSI_COLOR_YELLOW"   [ 아니오 ]" << std::endl;
-					
+					OnOff = false;					
 					break;
 			
+				case KEY_ENTER:
+					if (OnOff)
+					{
+						exitselect = false;
+						isGameRunning = false;
+					}
+					else{ Run(); }
+					break;
+
 				default:
 					isGameRunning = true;
-					break;
-					
+					break;					
 			}
-		}
-		
+		}		
 	};
-
 }
